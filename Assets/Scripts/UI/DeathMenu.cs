36bffa9ec7ace74bb55ee425f21184a3ce5cd52a
Baris Ikinci <58369631+brsikinci@users.noreply.github.com>
2023-05-07 01:34:12 +0300

    Death Menu

---
+++ Assets/Scripts/UI/DeathMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    [SerializeField] private Player Player;
+    public GameObject deathMenuUI;
+
+    void Update()
+    {
+        if (Player.Health == 0)
+        {
+            deathMenuUI.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        deathMenuUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnToMenu()
+    {
+        deathMenuUI.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -38,6 +38,6 @@ public class PauseMenu : MonoBehaviour
 
     public void ReturnToMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(0);
     }
 }
